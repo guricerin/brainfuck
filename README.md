b2b7fc728e90fb5e98eb2a7cb8a74805ea06dada
guricerin <gurizumi@gmail.com>
2020-03-19 15:45:22 +0900

    [fix]

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # brainfuck
 
+Brainfuck interpreter in F#
+
 ## Requirements
 .NET Core 3.1  
 https://dotnet.microsoft.com/download  
--- src/Brainfuck/Program.fs
+++ src/Brainfuck/Program.fs
@@ -40,14 +40,15 @@ let main argv =
         repl()
     | 1 -> 
         let filename = argv.[0]
-        let lines = File.ReadLines(filename)
-        for line in lines do
-            line |> Token.lex |> Instruction.parse |> machine.Run
+        let code = File.ReadAllLines(filename) |> String.concat ""
+        use writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()))
+        Console.SetOut(writer)
+        code |> Token.lex |> Instruction.parse |> machine.Run
     | _ -> 
         let msg = """
 Usage: 
 Brainfuck.exe -> REPL
-Brainfuck.exe <path-to-file> -> interpret target file
+Brainfuck.exe <path-to-bf-file> -> interpret target file
         """
         Console.WriteLine(msg)
         exit 1
